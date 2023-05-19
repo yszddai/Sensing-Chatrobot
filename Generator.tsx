04431d5049fb2fff35cfe44a66800aa520cb8894
yszddai <372767021@qq.com>
2023-05-19 14:52:31 +0800

    Add files via upload

---
+++ Generator.tsx
@@ -0,0 +1,352 @@
+import { Index, Show, createEffect, createSignal, onCleanup, onMount } from 'solid-js'
+import { useThrottleFn } from 'solidjs-use'
+import { generateSignature } from '@/utils/auth'
+import IconClear from './icons/Clear'
+import MessageItem from './MessageItem'
+import SystemRoleSettings from './SystemRoleSettings'
+import ErrorMessageItem from './ErrorMessageItem'
+import type { ChatMessage, ErrorMessage } from '@/types'
+
+
+export default () => {
+  let inputRef: HTMLTextAreaElement
+  const [currentSystemRoleSettings, setCurrentSystemRoleSettings] = createSignal('')
+  const [systemRoleEditing, setSystemRoleEditing] = createSignal(false)
+  const [messageList, setMessageList] = createSignal<ChatMessage[]>([])
+  const [currentError, setCurrentError] = createSignal<ErrorMessage>()
+  const [currentAssistantMessage, setCurrentAssistantMessage] = createSignal('')
+  const [loading, setLoading] = createSignal(false)
+  const [controller, setController] = createSignal<AbortController>(null)
+  const [isStick, setStick] = createSignal(false)
+  const [recording, setRecording] = createSignal(false);
+  const [audioChunks, setAudioChunks] = createSignal([]);
+
+
+
+
+  onMount(() => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      alert('Your browser does not support audio recording.');
+    }
+
+    let lastPostion = window.scrollY
+
+    window.addEventListener('scroll', () => {
+      const nowPostion = window.scrollY
+      nowPostion < lastPostion && setStick(false)
+      lastPostion = nowPostion
+    })
+
+    try {
+      if (localStorage.getItem('messageList'))
+        setMessageList(JSON.parse(localStorage.getItem('messageList')))
+
+      if (localStorage.getItem('systemRoleSettings'))
+        setCurrentSystemRoleSettings(localStorage.getItem('systemRoleSettings'))
+
+      if (localStorage.getItem('stickToBottom') === 'stick')
+        setStick(true)
+    } catch (err) {
+      console.error(err)
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload)
+    onCleanup(() => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    })
+  })
+
+  const handleBeforeUnload = () => {
+    localStorage.setItem('messageList', JSON.stringify(messageList()))
+    localStorage.setItem('systemRoleSettings', currentSystemRoleSettings())
+    isStick() ? localStorage.setItem('stickToBottom', 'stick') : localStorage.removeItem('stickToBottom')
+  }
+
+  const toggleRecording = () => {
+    if (recording()) {
+      const mediaRecorder = new MediaRecorder(mediaStream());
+      mediaRecorder.addEventListener('dataavailable', event => {
+        const formData = new FormData();
+        formData.append('audio', event.data, 'recording.mp3');
+        throw new Error('Request failed')
+
+        fetch('http://127.0.0.1:5000/recog', {
+          method: 'POST',
+          body: formData
+        });
+      });
+
+      mediaRecorder.addEventListener('stop', () => {
+      setAudioChunks([]);
+      setRecording(false);
+      });
+
+      mediaRecorder.stop();
+    } else {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          setMediaStream(stream);
+
+          const mediaRecorder = new MediaRecorder(stream);
+          mediaRecorder.start();
+
+          mediaRecorder.addEventListener('dataavailable', event => {
+            setAudioChunks([...audioChunks(), event.data]);
+          });
+
+          setRecording(true);
+        });
+    }
+  };
+
+  const [mediaStream, setMediaStream] = createSignal(null);
+
+
+
+  // const newrecorder = new windows.webkitSe()
+  //
+  // const recorder = new Recorder({
+  //   sampleRate: 44100,
+  //   numChannels: 2,
+  // });
+  //
+  // // 创建一个状态来存储录音状态
+  // const [isRecording, setIsRecording] = createSignal(false);
+  //
+  // // 处理录音按钮点击事件
+  // const handleRecordButtonClick = async () => {
+  //   if (isRecording()) {
+  //     // 停止录音
+  //     const audio = await recorder.stop();
+  //
+  //     // 更新 UI
+  //     setAudioData(audio);
+  //     inputRef.value = "正在识别.."
+  //
+  //   } else {
+  //     // 开始录音
+  //     await recorder.start();
+  //
+  //     // 更新 UI
+  //     setAudioData(null);
+  //     inputRef.value = "正在录音.."
+  //   }
+  //
+  //   // 切换录音状态
+  //   setIsRecording(!isRecording());
+  // };
+
+  const handleButtonClick = async() => {
+    const inputValue = inputRef.value
+    if (!inputValue)
+      return
+
+    inputRef.value = ''
+    setMessageList([
+      ...messageList(),
+      {
+        role: 'user',
+        content: inputValue,
+      },
+    ])
+    requestWithLatestMessage()
+    instantToBottom()
+  }
+
+  const smoothToBottom = useThrottleFn(() => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+  }, 300, false, true)
+
+  const instantToBottom = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'instant' })
+  }
+
+  const requestWithLatestMessage = async() => {
+    setLoading(true)
+    setCurrentAssistantMessage('')
+    setCurrentError(null)
+    const storagePassword = localStorage.getItem('pass')
+    try {
+      const controller = new AbortController()
+      setController(controller)
+      const requestMessageList = [...messageList()]
+      if (currentSystemRoleSettings()) {
+        requestMessageList.unshift({
+          role: 'system',
+          content: currentSystemRoleSettings(),
+        })
+      }
+      const timestamp = Date.now()
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        body: JSON.stringify({
+          messages: requestMessageList,
+          time: timestamp,
+          pass: storagePassword,
+          sign: await generateSignature({
+            t: timestamp,
+            m: requestMessageList?.[requestMessageList.length - 1]?.content || '',
+          }),
+        }),
+        signal: controller.signal,
+      })
+      if (!response.ok) {
+        const error = await response.json()
+        console.error(error.error)
+        setCurrentError(error.error)
+        throw new Error('Request failed')
+      }
+      const data = response.body
+      if (!data)
+        throw new Error('No data')
+
+      const reader = data.getReader()
+      const decoder = new TextDecoder('utf-8')
+      let done = false
+
+      while (!done) {
+        const { value, done: readerDone } = await reader.read()
+        if (value) {
+          const char = decoder.decode(value)
+          if (char === '\n' && currentAssistantMessage().endsWith('\n'))
+            continue
+
+          if (char)
+            setCurrentAssistantMessage(currentAssistantMessage() + char)
+
+          isStick() && instantToBottom()
+        }
+        done = readerDone
+      }
+    } catch (e) {
+      console.error(e)
+      setLoading(false)
+      setController(null)
+      return
+    }
+    archiveCurrentMessage()
+    isStick() && instantToBottom()
+  }
+
+  const archiveCurrentMessage = () => {
+    if (currentAssistantMessage()) {
+      setMessageList([
+        ...messageList(),
+        {
+          role: 'assistant',
+          content: currentAssistantMessage(),
+        },
+      ])
+      setCurrentAssistantMessage('')
+      setLoading(false)
+      setController(null)
+      inputRef.focus()
+    }
+  }
+
+  const clear = () => {
+    inputRef.value = ''
+    inputRef.style.height = 'auto'
+    setMessageList([])
+    setCurrentAssistantMessage('')
+    setCurrentError(null)
+  }
+
+  const stopStreamFetch = () => {
+    if (controller()) {
+      controller().abort()
+      archiveCurrentMessage()
+    }
+  }
+
+  const retryLastFetch = () => {
+    if (messageList().length > 0) {
+      const lastMessage = messageList()[messageList().length - 1]
+      if (lastMessage.role === 'assistant')
+        setMessageList(messageList().slice(0, -1))
+
+      requestWithLatestMessage()
+    }
+  }
+
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (e.isComposing || e.shiftKey)
+      return
+
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      handleButtonClick()
+    }
+  }
+
+  return (
+    <div my-6>
+      <SystemRoleSettings
+        canEdit={() => messageList().length === 0}
+        systemRoleEditing={systemRoleEditing}
+        setSystemRoleEditing={setSystemRoleEditing}
+        currentSystemRoleSettings={currentSystemRoleSettings}
+        setCurrentSystemRoleSettings={setCurrentSystemRoleSettings}
+      />
+      <Index each={messageList()}>
+        {(message, index) => (
+          <MessageItem
+            role={message().role}
+            message={message().content}
+            showRetry={() => (message().role === 'assistant' && index === messageList().length - 1)}
+            onRetry={retryLastFetch}
+          />
+        )}
+      </Index>
+      {currentAssistantMessage() && (
+        <MessageItem
+          role="assistant"
+          message={currentAssistantMessage}
+        />
+      )}
+      { currentError() && <ErrorMessageItem data={currentError()} onRetry={retryLastFetch} /> }
+      <Show
+        when={!loading()}
+        fallback={() => (
+          <div class="gen-cb-wrapper">
+            <span>The robot is thinking.....</span>
+            <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
+          </div>
+        )}
+      >
+        <div class="gen-text-wrapper" class:op-50={systemRoleEditing()}>
+          <textarea
+            ref={inputRef!}
+            disabled={systemRoleEditing()}
+            onKeyDown={handleKeydown}
+            placeholder="Input something..."
+            autocomplete="off"
+            autofocus
+            onInput={() => {
+              inputRef.style.height = 'auto'
+              inputRef.style.height = `${inputRef.scrollHeight}px`
+            }}
+            rows="1"
+            class="gen-textarea"
+          />
+          <button onClick={toggleRecording} disabled={systemRoleEditing()} gen-slate-btn>
+            {recording() ? 'Stop' : 'Start'}
+          </button>
+          <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
+            Send
+          </button>
+          <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
+            <IconClear />
+          </button>
+        </div>
+      </Show>
+      <div class="fixed bottom-5 left-5 rounded-md hover:bg-slate/10 w-fit h-fit transition-colors active:scale-90" class:stick-btn-on={isStick()}>
+        <div>
+          <button class="p-2.5 text-base" title="stick to bottom" type="button" onClick={() => setStick(!isStick())}>
+            <div i-ph-arrow-line-down-bold />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
