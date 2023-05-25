2642dc8d26b87b37a0e3068ae2d872d78fbd4868
yszddai <372767021@qq.com>
2023-05-25 16:31:05 +0800

    Add files via upload

---
+++ src/components/icons/record.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+      <circle fill="#FF0000" cx="12" cy="12" r="8" />
+    </svg>
+  )
+}
---
+++ src/components/icons/stop.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+      <rect fill="#000" x="4" y="4" width="16" height="16" />
+    </svg>
+  )
+}
